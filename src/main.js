6c2e354d1f4d32dcb123f2900e32b240735a8480
Victor Silva de Oliveira <victor.oliveira@almundo.com>
2018-03-26 13:23:10 +0000

    Adding some changes

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const a = 3;
+const b = 2;
+const d = 4;
+
+const c = 'asgdahdsagdajgdhajdghajsdhgasdjaghsdjagsdhajgdhasjdaghsdjahsdgajdghajsdhgasdjaghsdjgahsdajgsdhajgdajsgdhasjdaghsdjadhgasjd';
+
+function sum() {
+  return a + b + c + d;
+}
+
+sum();
